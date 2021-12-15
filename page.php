71daa20606c7a6cf167df367fdc78691e490d9fe
ShibutElizabeth <61345609+ShibutElizabeth@users.noreply.github.com>
2021-12-15 21:09:14 +0300

    posts

--- page.php
+++ page.php
@@ -5,25 +5,29 @@
 		</div>
 <div class="posts">
 	<div class="post">
-		
+		<div class="post__hat">
 			<img class="post__img" src="<?php bloginfo('template_url'); ?>/images/icon1.png"/>
-			<h2 class="post__title">CHECK OUT MY LATEST PORTFOLIO</h2>
-		
-		<p class="post__description">Decription</p>
+			<h2 class="font-title post__title">CHECK OUT MY LATEST PORTFOLIO</h2>
+		</div>
+		<p class="post__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius dolor, architecto dolorem aspernatur maxime incidunt exercitationem temporibus ducimus quibusdam sit iusto quae distinctio commodi! Ipsum qui repudiandae ut labore quasi!</p>
+		<button class="font-title post__button">MORE</button>
 	</div>
 	<div class="post">
-		<div></div>
+		<div class="post__hat">
 			<img class="post__img" src="<?php bloginfo('template_url'); ?>/images/icon2.png"/>
-			<h2 class="post__title">OFFER TOP NOTCH QUALITY</h2>
-		
-		<p class="post__description">Decription</p>
+			<h2 class="font-title post__title">OFFER TOP NOTCH QUALITY     WORK & SERVICES</h2>
+		</div>			
+		<p class="post__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora debitis eligendi nam facilis odio quam voluptates, laboriosam dolorem ut repellat fuga, quaerat doloribus illum, possimus enim. Quam nisi sapiente esse!</p>
+		<button class="font-title post__button">MORE</button>
 	</div>
 	<div class="post">
-		
+		<div class="post__hat">
 			<img class="post__img" src="<?php bloginfo('template_url'); ?>/images/icon3.png"/>
-			<h2 class="post__title">CHECK OUT MY LATEST PORTFOLIO</h2>
-		
-		<p class="post__description">Decription</p>
+			<h2 class="font-title post__title">FOLLOW ME ON TWITTER, FACEBOOK & PINTEREST</h2>
+		</div>
+		<!--   -->
+		<p class="post__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt qui sunt, accusantium dolorum labore possimus voluptate velit a necessitatibus ea natus reiciendis quod repellendus autem nostrum ex exercitationem nobis! Molestias.</p>
+		<button class="font-title post__button">MORE</button>
 	</div>
 	<section>
 	
